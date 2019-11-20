82501bc0a24fffad467f78d747a900dbe174bbad
MrNivorous <karn.sood@gmail.com>
2019-11-20 11:47:55 -0600

    added header component and links to shop and contact page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>, document.getElementById('root'));
 
 
